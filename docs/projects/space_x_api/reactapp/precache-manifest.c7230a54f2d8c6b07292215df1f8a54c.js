30535d385bbbaa3a44e0d783fe37bc1bd2c46d31
Jerry Crow <jercoinc@gmail.com>
2020-06-16 10:10:30 -0500

    Step 2 - fixing uppercase reactapp folder

---
+++ docs/projects/space_x_api/reactapp/precache-manifest.c7230a54f2d8c6b07292215df1f8a54c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c1c5d3d7465fdaa59e7055e826f13f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8572f8b214985304bf8d",
+    "url": "/static/css/main.1ebaa7d5.chunk.css"
+  },
+  {
+    "revision": "2beea615c3912b12590a",
+    "url": "/static/js/2.aa4c3ff4.chunk.js"
+  },
+  {
+    "revision": "8572f8b214985304bf8d",
+    "url": "/static/js/main.0a520f0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
